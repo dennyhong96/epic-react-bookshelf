3e64f7cf3da8d157f8e9f13a53bdcc902e39c7d6
Denny Hong <hong961127@gmail.com>
2022-05-11 23:05:02 -0700

    Use constants for colors and media queries

--- src/components/lib.exercise.js
+++ src/components/lib.exercise.js
@@ -1,25 +1,28 @@
 import styled from '@emotion/styled/macro' // macro import triggers babel macro to add displayName for styled compoennts automatically
 import {Dialog as ReachDialog} from '@reach/dialog'
 
+import * as colors from 'styles/colors'
+import * as mq from 'styles/media-queries'
+
 const Button = styled.button(({variant}) => ({
   padding: '10px 15px',
   border: '0',
   lineHeight: '1',
   borderRadius: '3px',
   ...(variant === 'primary' && {
-    background: '#3f51b5',
-    color: 'white',
+    background: colors.indigo,
+    color: colors.base,
   }),
   ...(variant === 'secondary' && {
-    background: '#f1f2f7',
-    color: '#434449',
+    background: colors.gray,
+    color: colors.text,
   }),
 }))
 
 const Input = styled.input({
   borderRadius: '3px',
-  border: '1px solid #f1f1f4',
-  background: '#f1f2f7',
+  border: `1px solid ${colors.gray10}`,
+  background: colors.gray,
   padding: '8px 12px',
 })
 
@@ -38,8 +41,8 @@ const CircleButton = styled.button({
   alignItems: 'center',
   justifyContent: 'center',
   background: 'white',
-  color: '#434449',
-  border: `1px solid #f1f1f4`,
+  color: colors.text,
+  border: `1px solid ${colors.gray10}`,
   cursor: 'pointer',
 })
 
@@ -49,7 +52,7 @@ const Dialog = styled(ReachDialog)({
   paddingBottom: '3.5em',
   boxShadow: '0 10px 30px -5px rgba(0, 0, 0, 0.2)',
   margin: '20vh auto',
-  '@media (max-width: 991px)': {
+  [mq.small]: {
     width: '100%',
     margin: '10vh auto',
   },
