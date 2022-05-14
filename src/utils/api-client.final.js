63cc808a0e1bd6ef8559be46b1e8165ab34d0852
Denny Hong <hong961127@gmail.com>
2022-05-14 10:25:36 -0700

    Finish data fetching

--- src/utils/api-client.final.js
+++ src/utils/api-client.final.js
@@ -1,14 +1,15 @@
-function client(endpoint, customConfig = {}) {
-  const config = {
-    method: 'GET',
-    ...customConfig,
+async function client(endpoint, customConfig = {}) {
+  const config = {method: 'GET', ...customConfig}
+  const res = await window.fetch(
+    `${process.env.REACT_APP_API_URL}/${endpoint}`,
+    config,
+  )
+  const data = await res.json()
+  if (!res.ok) {
+    return Promise.reject(new Error(res.statusText))
+  } else {
+    return data
   }
-  return window
-    .fetch(`${process.env.REACT_APP_API_URL}/${endpoint}`, config)
-    .then(res => {
-      if (!res.ok) return Promise.reject(new Error(res.statusText))
-      return res.json()
-    })
 }
 
 export {client}
